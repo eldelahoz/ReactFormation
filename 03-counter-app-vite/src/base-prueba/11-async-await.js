486092e1c0990bdce35c6ed05df6ff421d573401
eldelahoz <andelahoz13@gmail.com>
2024-07-24 16:00:09 -0500

    ADD: Pruebas async await

--- 03-counter-app-vite/src/base-prueba/11-async-await.js
+++ 03-counter-app-vite/src/base-prueba/11-async-await.js
@@ -1,24 +1,15 @@
-// const getImagenPromesa = () =>
-//   new Promise((resolve) => resolve("https://fasdfdsa.com"));
-
-// getImagenPromesa().then(console.log);
-
-const getImagen = async () => {
+export const getImagen = async () => {
   try {
     const apiKey = "NWpwzs5fFSkJv6jPNjnTJXL5Wxi4yUpt";
-
     const resp = await fetch(
       `http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`
     );
     const { data } = await resp.json();
-    const { url } = data.images.original;
-    const img = document.createElement("img");
-    img.src = url;
 
-    document.body.append(img);
+    const { url } = data.images.original;
+
+    return url;
   } catch (error) {
-    console.error(error);
+    return "No se encontro la imagen";
   }
 };
-
-getImagen();
