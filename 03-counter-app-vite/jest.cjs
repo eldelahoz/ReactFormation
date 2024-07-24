2c1af57aae722ce6c3ad7bf6dff99823b1c41793
eldelahoz <andelahoz13@gmail.com>
2024-07-24 18:03:28 -0500

    ADD: Test de primer component react

--- 03-counter-app-vite/jest.cjs
+++ 03-counter-app-vite/jest.cjs
@@ -1,7 +1,7 @@
-/** @returns {Promise<import('jest').Config>} */
-module.exports = async () => {
-  return {
-    verbose: true,
-    setupFiles: ["./jest.setup.js"],
-  };
+/** @type {import('jest').Config} */
+const config = {
+  testEnvironment: "jsdom",
+  verbose: true,
+  setupFiles: ["./jest.setup.js"],
 };
+module.exports = config;
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles.css";
-import { CounterApp } from "./CounterApp";
+// import { CounterApp } from "./CounterApp";
+import { FirstApp } from "./FirstApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CounterApp value={20} />
+    <FirstApp title="Hola, Soy Luffy" />
+    {/* <CounterApp value={20} /> */}
   </React.StrictMode>
 );
