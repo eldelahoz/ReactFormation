019879205fa053b05002a49fa379f6ed5d2cc37b
eldelahoz <andelahoz13@gmail.com>
2024-08-01 10:42:30 -0500

    ADD: Nuevo componente para el input

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["Luffy", "HxH"]);
+
+  const onAddCategory = () => {
+    setCategories(["hi", ...categories]);
+  };
+  console.log(categories);
   return (
     <>
+      {/* titulo */}
       <h1>GifExpertApp</h1>
+
+      {/* Input */}
+      <AddCategory />
+      {/* Listado de Gif */}
+      <button onClick={onAddCategory}>Agregar</button>
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
+      {/* Gif Item */}
     </>
   );
 };
