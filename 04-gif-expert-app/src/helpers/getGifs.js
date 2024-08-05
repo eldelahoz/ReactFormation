d3d91944bde33c75fdd2c66a54bf65cd2a0bc277
eldelahoz <andelahoz13@gmail.com>
2024-08-05 11:12:09 -0500

    ADD: Hoooks personalizado

--- 04-gif-expert-app/src/helpers/getGifs.js
+++ 04-gif-expert-app/src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=NWpwzs5fFSkJv6jPNjnTJXL5Wxi4yUpt&q=${category}&limit=20`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=NWpwzs5fFSkJv6jPNjnTJXL5Wxi4yUpt&q=${category}&limit=10`;
 
   const resp = await fetch(url);
 
@@ -10,6 +10,5 @@ export const getGifs = async (category) => {
     title: img.title,
     url: img.images.downsized_medium.url,
   }));
-  console.log(gifs);
   return gifs;
 };
